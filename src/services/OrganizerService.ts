1daae99421d752394cd6230df1a7aa397bf833b4
JJKung07 <jiraphat_l@cmu.ac.th>
2024-10-11 10:30:56 +0700

    Lab 12 #2

--- src/services/OrganizerService.ts
+++ src/services/OrganizerService.ts
@@ -1,14 +1,5 @@
 import type { Organizer } from '@/types'
-import axios, { type AxiosResponse } from 'axios'
-
-const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL as string,
-  withCredentials: false,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
-})
+import apiClient from './AxiosClient'
 
 export default {
   getOrganizers() {
